7f3c0efc5248310d4e53cf99f885cfaf7a242c3b
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2023-05-11 05:54:07 -0300

    vs1.006

--- code/.js
+++ code/.js
@@ -1,31 +1,58 @@
+
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const slideWidth = slides[0].offsetWidth;
 
-let position = 0;
 let isDragging = false;
-let startPos = 0;
+let startPosition = 0;
+let currentTranslate = 0;
+let previousTranslate = 0;
+let animationId = 0;
 
-slider.addEventListener('touchstart', e => {
-  isDragging = true;
-  startPos = e.touches[0].clientX;
+slides.forEach((slide, index) => {
+  // Touch events
+  slide.addEventListener('pointerdown', e => {
+    isDragging = true;
+    startPosition = e.clientX || e.touches[0].clientX;
+    currentTranslate = -index * slideWidth;
+    previousTranslate = currentTranslate;
+    cancelAnimationFrame(animationId);
+  });
+
+  slide.addEventListener('pointermove', e => {
+    if (isDragging) {
+      const currentPosition = e.clientX || e.touches[0].clientX;
+      const diff = currentPosition - startPosition;
+      currentTranslate = previousTranslate + diff;
+      setSliderPosition();
+    }
+  });
+
+  slide.addEventListener('pointerup', () => {
+    isDragging = false;
+    cancelAnimationFrame(animationId);
+    const slideIndex = Math.round(-currentTranslate / slideWidth);
+    currentTranslate = -slideIndex * slideWidth;
+    previousTranslate = currentTranslate;
+    setSliderPosition();
+  });
+
+  slide.addEventListener('pointerleave', () => {
+    if (isDragging) {
+      isDragging = false;
+      cancelAnimationFrame(animationId);
+      const slideIndex = Math.round(-currentTranslate / slideWidth);
+      currentTranslate = -slideIndex * slideWidth;
+      previousTranslate = currentTranslate;
+      setSliderPosition();
+    }
+  });
 });
 
-slider.addEventListener('touchmove', e => {
-  if (isDragging) {
-    const currentPosition = e.touches[0].clientX;
-    const distance = currentPosition - startPos;
-    position = position + distance;
-    slider.style.transform = `translateX(${position}px)`;
-  }
-});
-
-slider.addEventListener('touchend', () => {
-  isDragging = false;
-  const slideIndex = Math.round(-position / slideWidth);
-  position = -slideIndex * slideWidth;
-  slider.style.transform = `translateX(${position}px)`;
-});
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+  animationId = requestAnimationFrame(setSliderPosition);
+}
 
 
 
