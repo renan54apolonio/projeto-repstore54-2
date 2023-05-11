811e68d9de040fa54cd87d6ae11dafad62b14ac1
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2023-05-11 05:47:37 -0300

    vs1.004

--- code/.js
+++ code/.js
@@ -1,81 +1,31 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].offsetWidth;
 
-const nav = document.querySelector('nav');
-const navLinks = document.querySelectorAll('nav a');
-const navLinksCount = navLinks.length;
-
-// Clone o menu e adicione-o ao final para criar um efeito de slider infinito
-nav.innerHTML += nav.innerHTML;
-
+let position = 0;
 let isDragging = false;
-let startX, scrollLeft;
+let startPos = 0;
 
-nav.addEventListener('mousedown', (e) => {
+slider.addEventListener('touchstart', e => {
   isDragging = true;
-  startX = e.pageX - nav.offsetLeft;
-  scrollLeft = nav.scrollLeft;
-  nav.classList.add('active');
+  startPos = e.touches[0].clientX;
 });
 
-nav.addEventListener('mouseup', () => {
-  isDragging = false;
-  nav.classList.remove('active');
-  snapToClosestNavItem();
-});
-
-nav.addEventListener('mousemove', (e) => {
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.pageX - nav.offsetLeft;
-  const walk = (x - startX) * 3;
-  nav.scrollLeft = scrollLeft - walk;
-});
-
-nav.addEventListener('touchstart', (e) => {
-  isDragging = true;
-  startX = e.touches[0].clientX - nav.offsetLeft;
-  scrollLeft = nav.scrollLeft;
-  nav.classList.add('active');
-});
-
-nav.addEventListener('touchend', () => {
-  isDragging = false;
-  nav.classList.remove('active');
-  snapToClosestNavItem();
-});
-
-nav.addEventListener('touchmove', (e) => {
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.touches[0].clientX - nav.offsetLeft;
-  const walk = (x - startX) * 3;
-  nav.scrollLeft = scrollLeft - walk;
-});
-
-function snapToClosestNavItem() {
-  // Encontre o índice do item mais próximo do centro do menu
-  let closestIndex = 0;
-  let closestDistance = Infinity;
-  const center = nav.offsetWidth / 2;
-  navLinks.forEach((link, i) => {
-    const distance = Math.abs(link.offsetLeft - center);
-    if (distance < closestDistance) {
-      closestIndex = i;
-      closestDistance = distance;
-    }
-  });
-
-  // Se o item mais próximo for um item clonado, mude o índice para o item original correspondente
-  if (closestIndex >= navLinksCount) {
-    closestIndex -= navLinksCount;
+slider.addEventListener('touchmove', e => {
+  if (isDragging) {
+    const currentPosition = e.touches[0].clientX;
+    const distance = currentPosition - startPos;
+    position = position + distance;
+    slider.style.transform = `translateX(${position}px)`;
   }
+});
 
-  // Rola para o item mais próximo
-  const closestLink = navLinks[closestIndex];
-  nav.scrollTo({
-    left: closestLink.offsetLeft - nav.offsetWidth / 2 + closestLink.offsetWidth / 2,
-    behavior: 'smooth'
-  });
-}
+slider.addEventListener('touchend', () => {
+  isDragging = false;
+  const slideIndex = Math.round(-position / slideWidth);
+  position = -slideIndex * slideWidth;
+  slider.style.transform = `translateX(${position}px)`;
+});
 
 
 
