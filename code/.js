23bc018cd3e452e2573e4a76c28d3a5026e983c1
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2023-05-15 08:01:36 -0300

    vai pelo amor de deus

--- code/.js
+++ code/.js
@@ -1,56 +1,20 @@
-const nav = document.querySelector('nav');
-const navLinks = document.querySelectorAll('nav a');
-const navLinksCount = navLinks.length;
+let container = document.querySelector(".container");
+let buttons = document.querySelectorAll(".button");
+let startX, endX;
 
-let isDragging = false;
-let startX, scrollLeft;
-
-nav.addEventListener('touchstart', (e) => {
-  isDragging = true;
-  startX = e.touches[0].clientX - nav.offsetLeft;
-  scrollLeft = nav.scrollLeft;
-  nav.classList.add('active');
+container.addEventListener("touchstart", e => {
+	startX = e.touches[0].clientX;
 });
 
-nav.addEventListener('touchend', () => {
-  isDragging = false;
-  nav.classList.remove('active');
-  snapToClosestNavItem();
+container.addEventListener("touchmove", e => {
+	endX = e.touches[0].clientX;
+	let diffX = endX - startX;
+	container.scrollLeft -= diffX;
+	startX = endX;
 });
 
-nav.addEventListener('touchmove', (e) => {
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.touches[0].clientX - nav.offsetLeft;
-  const walk = (x - startX) * 3;
-  nav.scrollLeft = scrollLeft - walk;
+container.addEventListener("touchend", e => {
+	endX = e.changedTouches[0].clientX;
+	let diffX = endX - startX;
+	container.scrollLeft -= diffX;
 });
-
-function snapToClosestNavItem() {
-  // Encontre o índice do item mais próximo do centro do menu
-  let closestIndex = 0;
-  let closestDistance = Infinity;
-  const center = nav.offsetWidth / 2;
-  navLinks.forEach((link, i) => {
-    const distance = Math.abs(link.offsetLeft - center);
-    if (distance < closestDistance) {
-      closestIndex = i;
-      closestDistance = distance;
-    }
-  });
-
-  // Se o item mais próximo for um item clonado, mude o índice para o item original correspondente
-  if (closestIndex >= navLinksCount) {
-    closestIndex -= navLinksCount;
-  }
-
-  // Rola para o item mais próximo
-  const closestLink = navLinks[closestIndex];
-  nav.scrollTo({
-    left: closestLink.offsetLeft - nav.offsetWidth / 2 + closestLink.offsetWidth / 2,
-    behavior: 'smooth'
-  });
-}
-
-// Habilita o deslocamento horizontal durante o toque
-nav.style.touchAction = 'pan-y';
