1f9b5c9a23f2fddbc118fc1b63c2443a89e6bb9c
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2023-05-15 10:01:41 -0300

    jesus me ajude

--- code/.js
+++ code/.js
@@ -1,32 +1,17 @@
-let container = $(".container");
-let inner = $(".inner");
-let isDragging = false;
-let startClientX, startPageX, currentX = 0;
+var touchstartX = 0;
+var touchendX = 0;
+var distanceX = 0;
 
-inner.on("touchstart", e => {
-	isDragging = true;
-	startClientX = e.touches[0].clientX;
-	startPageX = e.touches[0].pageX;
-	currentX = inner.offset().left;
+$('.inner').on('touchstart', function(event) {
+    touchstartX = event.originalEvent.touches[0].screenX;
 });
 
-inner.on("touchmove", e => {
-	if (!isDragging) return;
-	let currentClientX = e.touches[0].clientX;
-	let currentDeltaX = currentClientX - startClientX;
-	let currentPageX = e.touches[0].pageX;
-	let currentDeltaPageX = currentPageX - startPageX;
-	let newX = currentX + currentDeltaPageX;
-	if (newX > 0) newX = 0;
-	if (newX < - (inner.width() - container.width())) newX = - (inner.width() - container.width());
-	inner.css({ left: newX });
-	e.preventDefault();
+$('.inner').on('touchmove', function(event) {
+    touchendX = event.originalEvent.touches[0].screenX;
+    distanceX = touchstartX - touchendX;
+    $(this).css('transform', 'translateX(' + -distanceX + 'px)');
 });
 
-inner.on("touchend", e => {
-	isDragging = false;
-});
-
-inner.on("touchcancel", e => {
-	isDragging = false;
+$('.inner').on('touchend', function(event) {
+    $(this).css('transform', 'translateX(0)');
 });
