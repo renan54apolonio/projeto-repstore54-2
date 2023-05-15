14ec69f65c7767d0a343efb420d039693f3c95a9
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2023-05-15 10:28:06 -0300

    mona lisa

--- code/.js
+++ code/.js
@@ -2,6 +2,25 @@ var inner = document.querySelector('.inner');
 var touchstartX = 0;
 var touchendX = 0;
 var distanceX = 0;
+var mousedownX = 0;
+var mouseupX = 0;
+
+inner.addEventListener('mousedown', function(event) {
+    mousedownX = event.screenX;
+});
+
+inner.addEventListener('mousemove', function(event) {
+    if (mousedownX !== 0) {
+        mouseupX = event.screenX;
+        distanceX = mousedownX - mouseupX;
+        inner.style.transform = 'translateX(' + -distanceX + 'px)';
+    }
+});
+
+inner.addEventListener('mouseup', function(event) {
+    mousedownX = 0;
+    inner.style.transform = 'translateX(0)';
+});
 
 inner.addEventListener('touchstart', function(event) {
     touchstartX = event.touches[0].screenX;
@@ -16,3 +35,4 @@ inner.addEventListener('touchmove', function(event) {
 inner.addEventListener('touchend', function(event) {
     inner.style.transform = 'translateX(0)';
 });
+
