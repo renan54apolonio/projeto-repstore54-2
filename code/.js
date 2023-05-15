2dd45d0809fa379896a4c7de0b99cd32067fc8ec
renan54apolonio <119420420+renan54apolonio@users.noreply.github.com>
2023-05-15 09:39:57 -0300

    mouse fdp

--- code/.js
+++ code/.js
@@ -3,23 +3,23 @@ let buttons = $(".button");
 let isDragging = false;
 let startX, scrollLeft;
 
-container.on("mousedown", e => {
+container.on("touchstart", e => {
 	isDragging = true;
-	startX = e.pageX - container.offset().left;
+	startX = e.touches[0].clientX - container.offset().left;
 	scrollLeft = container.scrollLeft();
 });
 
-container.on("mousemove", e => {
+container.on("touchmove", e => {
 	if (!isDragging) return;
-	let x = e.pageX - container.offset().left;
+	let x = e.touches[0].clientX - container.offset().left;
 	let walk = (x - startX) * 2;
 	container.scrollLeft(scrollLeft - walk);
 });
 
-container.on("mouseup", e => {
+container.on("touchend", e => {
 	isDragging = false;
 });
 
-container.on("mouseleave", e => {
+container.on("touchcancel", e => {
 	isDragging = false;
 });
